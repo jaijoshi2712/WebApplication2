a3b3b104d5a2c02e1c366f32db4ac3dee215de8a
jjoshi16 <jjoshi16@LHTU05CG2114XCV>
2022-07-15 15:25:57 +0530

    Add project files.

---
+++ WebApplication2/Controllers/SuperHeroController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "NYC"
+                }
+            };
+            return Ok(heroes);
+        }
+        
+
+
+
+
+    }
+}
